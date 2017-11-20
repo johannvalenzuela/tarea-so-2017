7a4ea33224576081f61447ee1963a9618ba399d1
Johann <johann.aevt@gmail.com>
2017-11-19 23:04:35 -0300

    Primero

---
+++ main.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <pthread.h>
+
+int working = 1;
+int is_safe(int rows[8], int x, int y){
+    int i;
+    
+    if (y == 0)
+            return 1;
+    for (i=0; i < y; ++i) {
+       if (rows[i] == x || rows[i] == x + y - i || rows[i] == x - y +i)
+            return 0;
+    }
+
+    return 1;
+}
+ 
+void putboard(int rows[8]){
+    int s = 0;
+    int x, y;
+
+    printf("\nSolution #%d:\n---------------------------------\n", ++s);
+    for (y=0; y < 8; ++y) {
+        for (x=0; x < 8; ++x)
+            printf(x == rows[y] ? "| Q " : "|   ");
+        printf("|\n---------------------------------\n");
+    }
+}
+ 
+void eight_queens_helper(int rows[8], int y){
+    int x;
+    for (x=0; x < 8; ++x) {
+        if (is_safe(rows, x, y)) {
+            rows[y] = x;
+            if (y == 7){
+              putboard(rows);
+              working = 0;
+            }
+            else
+              eight_queens_helper(rows, y+1);
+        }
+    }
+}
+
+void* board(){
+    printf("Mi numero es: %d \n",pthread_self);
+    int rows[8];
+    eight_queens_helper(rows, 0);
+}
+
+int main(){
+    pthread_t hilo;
+    while(working){
+        pthread_create(&hilo,NULL,board,NULL);
+        
+    }
+    return 0;
+}
